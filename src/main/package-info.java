09e5adc2b7bcff0eb1f51ee5883ba24b88d3c231
Jguer <me@jguer.space>
2018-05-05 22:10:56 +0100

    Add package info sample
    Signed-off-by: Jguer <me@jguer.space>

---
+++ src/main/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package implements a sample application to
+ * test our other packages.
+ */
+package main;
---
+++ src/optProblem/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * optProblem implements an optimization problem based on the pec and parser package
+ */
+package optProblem;
